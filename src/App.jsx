db18c72d2acbc1b7f242d4dac2612c87f9a03ef7
severogianluca <gianlucasevero300@gmail.com>
2025-04-28 05:44:04 +0200

    Feat: create posts card

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import DefaultLayout from './layout/DefaultLayout'
 import HomePage from './pages/HomePage'
+import ChiSiamo from './pages/ChiSiamo'
+import ListaDeiPost from './pages/ListaDeiPost'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={DefaultLayout}>
-          <Route index element={<HomePage />} />
-
+          <Route path='/' element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/ChiSiamo' element={<ChiSiamo />} />
+            <Route path='/ListaDeiPost' element={<ListaDeiPost />} />
           </Route>
         </Routes>
-      
       </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -3,6 +3,7 @@ function ChiSiamo() {
         <>
             <div className="container">
                 <h1 className="text-center">Chi siamo</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore sed accusantium harum at, aliquam, fugit soluta optio enim necessitatibus magnam voluptates commodi asperiores recusandae? Hic nulla architecto sint saepe excepturi?</p>
             </div>
         </>
     )
